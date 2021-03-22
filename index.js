78934ff55ddb59b0b007af440308b310fd86bda3
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-03-22 03:25:29 -0500

    commit tree c8a654a10101e83f6b03b21568c88a5be3158f47

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const people = ["Ada", "Brendan", "Ali"];
+let newPeople = []
+
+function writeCards(people) {
+  for (let i = 0; i < people.length; i++) {
+    console.log(`Thank you, ${people[i]}, for the wonderful surprise gift!`);
+    newPeople.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`);
+  }
+  return newPeople
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
